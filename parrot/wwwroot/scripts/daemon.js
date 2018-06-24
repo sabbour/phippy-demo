2473eff62617595c0a4b2040beeff814816e44f0
bradygmsft <brady.gaster@gmail.com>
2018-06-24 16:35:23 -0700

    cards now appearing!

--- parrot/wwwroot/scripts/daemon.js
+++ parrot/wwwroot/scripts/daemon.js
@@ -1,3 +1,5 @@
+const pods = [];
+
 const connection = new signalR.HubConnectionBuilder()
     .withUrl('/daemonhub', { 
         logger: signalR.LogLevel.Verbose 
@@ -6,17 +8,18 @@ const connection = new signalR.HubConnectionBuilder()
 
 connection.onclose(() => console.log('closed'));
 
-connection.on("userCountUpdated", (userCount) => { 
-    const userCountUi = document.getElementById("userCount");
-    userCountUi.textContent = userCount;
-});
-
 connection.on('clusterViewUpdated', (pod) => {
     console.log(pod);
     var li = document.createElement('li');
     var textNode = document.createTextNode(pod.name + ' namespace: (' + pod.nameSpace + ') ' + pod.action);
     li.appendChild(textNode);
     document.getElementById('pods').appendChild(li);
+
+    var source = $('#podCardTemplate').html();
+    var template = Handlebars.compile(source);
+    var result = template(pod);
+    console.log(result);
+    $('#podcards').append(result);
 });
 
 connection
